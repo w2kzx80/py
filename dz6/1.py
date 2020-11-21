df3356ecc56aeb5f07f3b0be4f40a4c5c7a0725e
Apanovici Roman <w2kzx80@gmail.com>
2020-11-21 19:34:39 +0200

    hw5

---
+++ dz6/1.py
@@ -0,0 +1,24 @@
+import time
+
+class TrafficLight:
+    def __init__(self):
+        self.__color = "red"
+        self.__intervals = [
+            {"color": "red", "interval": 7},
+            {"color": "yellow", "interval": 2},
+            {"color": "green", "interval": 10},
+        ]
+        self.__i = 0
+
+    def running(self):
+        while True:
+            self.__color=self.__intervals[self.__i]['color']
+            print(self.__color)
+            time.sleep(self.__intervals[self.__i]['interval'])
+            self.__i += 1
+            if self.__i >= len(self.__intervals):
+                self.__i = 0
+
+
+myTL = TrafficLight()
+myTL.running()
---
+++ dz6/4.py
@@ -0,0 +1,10 @@
+
+class Car:
+    def __inint__(self,speed, color, name, is_police):
+        self.speed=speed
+        self.color=color
+        self.name=name
+        self.is_police=is_police
+        self.direction=""
+
+    def go
