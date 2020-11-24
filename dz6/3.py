fc527f1590c3b0a7e01ad4f92ec482064b011cf5
Apanovici Roman <w2kzx80@gmail.com>
2020-11-24 21:21:41 +0200

    hw6

--- dz6/3.py
+++ dz6/3.py
@@ -1,10 +1,10 @@
 
 class Worker:
     def __init__(self, name, surname, position, wage, bonus):
-        self.name=name
-        self.surname=surname
-        self.position=position
-        self._income={"wage":wage,"bonus":bonus}
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = { "wage":wage, "bonus":bonus }
 
 class Position(Worker):
     def __init__(self, name, surname, position, wage, bonus):
@@ -14,8 +14,8 @@ class Position(Worker):
         return f"{self.name} {self.surname}"
 
     def get_total_income(self):
-        return self._income['wage']+self._income['bonus']
+        return self._income['wage'] + self._income['bonus']
 
-myP=Position("Roman","Apanovici","developer",2000,500)
+myP = Position("Roman", "Apanovici", "developer", 2000, 500)
 print(myP.get_full_name())
 print(myP.get_total_income())
